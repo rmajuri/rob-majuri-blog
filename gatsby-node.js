82db9f298eb91b0d947546cfe26b664de68478ef
Rob Majuri <robmajuri@gmail.com>
2019-10-06 20:36:05 -0500

    Don't render posts with null fields

--- gatsby-node.js
+++ gatsby-node.js
@@ -40,15 +40,17 @@ exports.createPages = async ({ graphql, actions }) => {
     const previous = index === posts.length - 1 ? null : posts[index + 1].node
     const next = index === 0 ? null : posts[index - 1].node
 
-    createPage({
-      path: post.node.fields.slug,
-      component: blogPost,
-      context: {
-        slug: post.node.fields.slug,
-        previous,
-        next,
-      },
-    })
+    if (post.node.frontmatter.template !== "page") {
+      createPage({
+        path: post.node.fields.slug,
+        component: blogPost,
+        context: {
+          slug: post.node.fields.slug,
+          previous,
+          next,
+        },
+      })
+    }
   })
 }
 
